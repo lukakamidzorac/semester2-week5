e809e1d23395e4b06761487f5162b444346b71a5
lukakamidzorac <sjvj0907@leeds.ac.uk>
2026-02-23 11:39:16 +0000

    end of sess 2.1

--- session_1/task2/task2.1.c
+++ session_1/task2/task2.1.c
@@ -30,34 +30,50 @@ int main(int argc, char *argv[]) {
     float num1, num2;
     char operation;
     float result;
-    
+
     // Check if the correct number of arguments is provided
     if (argc != 4) {
         printf("Usage: %s number1 operation number2\n", argv[0]);
         printf("Example: %s 5.0 + 3.5\n", argv[0]);
-        printf("Operations: +, -, x, /\n");  // Note 'x' instead of '*'
+        printf("Operations: +, -, x, /\n");
         return 1;
     }
-    
+
     // Convert command-line arguments to numbers and operation
-    // atof() converts a string (ASCII) to a floating-point (float) number
-    num1 = atof(argv[1]);  // Convert first argument to float
-    operation = argv[2][0];  // Just take the first character of the second argument
-    num2 = atof(argv[3]);  // Convert third argument to float
-    
-    // Complete your code here
-	switch(operation){
-		case :
+    num1 = atof(argv[1]);
+    operation = argv[2][0];
+    num2 = atof(argv[3]);
+
+    switch (operation) {
+        case '+':
+            result = num1 + num2;
             break;
-		default:
-			printf("Operation not defined!\n");
-	}
-    
+
+        case '-':
+            result = num1 - num2;
+            break;
+
+        case 'x':
+            result = num1 * num2;
+            break;
+
+        case '/':
+            if (num2 == 0) {
+                printf("Error, division by zero!\n");
+                return 1;
+            }
+            result = num1 / num2;
+            break;
+
+        default:
+            printf("Operation not defined!\n");
+            return 1;
+    }
+
     printf("Result: %.2f\n", result);
-    
+
     return 0;
 }
-
 
 /*
  * Advanced tasks:
