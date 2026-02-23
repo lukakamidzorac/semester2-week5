c21ab3960750bf38b5c3ac5595759b0ba00288b5
lukakamidzorac <sjvj0907@leeds.ac.uk>
2026-02-23 11:30:49 +0000

    end of session 1.3

--- session_1/task1/task1.3.c
+++ session_1/task1/task1.3.c
@@ -15,8 +15,16 @@
 #include <stdio.h>
 
 int main(void) {
+    int number;
 
-	// complete your code here
+    printf("Enter a number: ");
+    scanf("%d", &number);
 
-	return 0;
+    if ((number % 3 == 0) && (number % 5 == 0)) {
+        printf("The number is divisible by 3 and 5\n");
+    } else {
+        printf("The number is not divisible by both 3 and 5\n");
+    }
+
+    return 0;
 }
